16237d663fcd4270cb9b32a153db897f30ae1f67
ryanp <ryanpanimation@gmail.com>
2020-10-05 23:08:30 -0500

    single number test OK

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -1,14 +1,25 @@
-'''
+"""
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
-'''
+"""
+
+
 def single_number(arr):
     # Your code here
+    arr = sorted(arr)
+    # print(arr)
+    i = 0
+    while i < len(arr):
+        try:
+            if arr[i] == arr[i + 1]:
+                i += 2
+            else:
+                return arr[i]
+        except:
+            return arr[i]
 
-    pass
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Use the main function to test your implementation
     arr = [1, 1, 4, 4, 5, 5, 3, 3, 9, 0, 0]
 
